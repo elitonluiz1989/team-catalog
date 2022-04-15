0f5e3bdc9dd2c49279d50afa582fa7381a74b20c
elitonluiz1989 <elitonluiz1989@gmail.com>
2022-04-14 22:21:38 -0400

    Added browserSync configuration

--- webpack.mix.js
+++ webpack.mix.js
@@ -12,4 +12,12 @@ const mix = require('laravel-mix');
  */
 
 mix.js('resources/js/app.js', 'public/js')
-    .sass('resources/scss/app.scss', 'public/css');
+    .sass('resources/scss/app.scss', 'public/css')
+    .browserSync({
+        proxy : 'team-catalog.php',
+        host: 'localhost',
+        open: false,
+        port: 8080,
+        logConnections: true,
+        logLevel: 'debug',
+    });
